447e6d6f1867e0c8b63cb1241ab95163e3bd43a6
gui1949 <guilhermesantiago12@gmail.com>
2021-01-24 14:12:01 -0300

    Upload inicial

---
+++ precache-manifest.3d120f6df838539024cef3585fcf243f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24c705636d192a353876a547a5429ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2abe7e941141615be610",
+    "url": "./static/css/main.ff393bb9.chunk.css"
+  },
+  {
+    "revision": "12b5267b8531f716bb6d",
+    "url": "./static/js/2.9072eaca.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "./static/js/2.9072eaca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abe7e941141615be610",
+    "url": "./static/js/main.85d8e94c.chunk.js"
+  },
+  {
+    "revision": "6d5fb162623087c0e922",
+    "url": "./static/js/runtime-main.25c5193c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
