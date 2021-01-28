87b6d8d6870e2d8d4133da1da259fdb4d2e6f30d
Guilherme <guilhermesantiago12@gmail.com>
2021-01-28 14:30:52 -0300

    Adicionado o bot maligno do mal

--- index.js
+++ index.js
@@ -302,9 +302,118 @@ restapi.post("/data/upload", upload.single("photo"), (req, res) => {
   sendNotification(message);
 });
 
-//var server = https.createServer(options, restapi);
+restapi.post("/data/upload_bot", upload.single("photo"), (req, res) => {
+  let date_ob = new Date();
+
+  let day = ("0" + date_ob.getDate()).slice(-2);
+
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+  let year = date_ob.getFullYear();
+
+  let hours = date_ob.getHours();
+
+  let minutes = date_ob.getMinutes();
+  var data = {
+    USERNAME: req.body.username,
+    USER_PIC: req.body.photo,
+    POST_DATA: day + "/" + month + "/" + year + " - " + hours + ":" + minutes,
+    PIC_LOCAL: req.body.photo,
+    POST_DESC: req.body.description,
+    POST_LIKE: 0,
+    POST_DISLIKE: 0,
+  };
+  var sql =
+    "INSERT INTO POSTS (USERNAME,USER_PIC,POST_DATA,PIC_LOCAL,POST_DESC,POST_LIKE,POST_DISLIKE) VALUES(?,?,?,?,?,?,?)";
+  var params = [
+    data.USERNAME,
+    data.USER_PIC,
+    data.POST_DATA,
+    data.PIC_LOCAL,
+    data.POST_DESC,
+    data.POST_LIKE,
+    data.POST_DISLIKE,
+  ];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      console.log(data.USERNAME);
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      data: rows,
+    });
+  });
+
+  var sendNotification = function (data) {
+    var headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: "Basic NmFmNmZhZTMtMzRlMC00MzFjLTk5MWUtMWI3NWY3ZThmMzMw",
+    };
+
+    var options = {
+      host: "onesignal.com",
+      port: 443,
+      path: "/api/v1/notifications",
+      method: "POST",
+      headers: headers,
+    };
+
+    var https = require("https");
+    var req = https.request(options, function (res) {
+      res.on("data", function (data) {
+        console.log("Response:");
+        console.log(JSON.parse(data));
+      });
+    });
+
+    req.on("error", function (e) {
+      console.log("ERROR:");
+      console.log(e);
+    });
+
+    req.write(JSON.stringify(data));
+    req.end();
+  };
+
+  var message = {
+    app_id: "aeb277cc-35e9-4ec4-84a4-406fc5a78c34",
+    contents: { en: data.USERNAME + ", o mestre, postou la, vey" },
+    web_url: 'https://gui1949.github.io/BardoJeiz',
+    included_segments: ["Subscribed Users"],
+  };
+
+  sendNotification(message);
+});
 
 restapi.listen(process.env.PORT || 80);
-//server.listen(443);
 
-console.log("Submit GET or POST to http://localhost:80/data");
+
+console.log("Rest API Online");
+
+function bot_jeiz() {
+  setInterval(() => {
+    var input = ".source_bot/src1.jpg"    
+    var name = "Jeiz";
+    var desc = "Hoje to feliz rapaziada, pique beiçola";
+    let apiUrl = "https://bardojeiz-server.herokuapp.com/data/upload_bot";
+
+    var FormData = require("form-data");
+    var data = new FormData();
+    const fetch = require("node-fetch");
+
+    data.append("photo", input);
+    data.append("description", desc);
+    data.append("username", name);
+
+    fetch(apiUrl, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      if (response.ok) {
+        console.log("Bot rodou");
+      }
+    });
+  }, 3000);
+}
+bot_jeiz();
