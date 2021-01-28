bbffc99306b5ff51ce1eaf8881953edc0e077760
Guilherme <guilhermesantiago12@gmail.com>
2021-01-28 14:49:10 -0300

    Bot - Teste 03

--- bot.js
+++ bot.js
@@ -1,26 +1,26 @@
-// function bot_jeiz() {
-//   setInterval(() => {
-//     var input = "source_bot/src1.jpg"    
-//     var name = "Jeiz";
-//     var desc = "Hoje to feliz rapaziada, pique beiçola";
-//     let apiUrl = "https://bardojeiz-server.herokuapp.com/data/upload_bot";
+function bot_jeiz() {
+  setInterval(() => {
+    var input = "source_bot/src1.jpg"    
+    var name = "Jeiz";
+    var desc = "Hoje to feliz rapaziada, pique beiçola";
+    let apiUrl = "https://bardojeiz-server.herokuapp.com/data/upload_bot";
 
-//     var FormData = require("form-data");
-//     var data = new FormData();
-//     const fetch = require("node-fetch");
+    var FormData = require("form-data");
+    var data = new FormData();
+    const fetch = require("node-fetch");
 
-//     data.append("photo", input);
-//     data.append("description", desc);
-//     data.append("username", name);
+    data.append("photo", input);
+    data.append("description", desc);
+    data.append("username", name);
 
-//     fetch(apiUrl, {
-//       method: "POST",
-//       body: data,
-//     }).then(function (response) {
-//       if (response.ok) {
-//         console.log("Bot rodou");
-//       }
-//     });
-//   }, 4000000);
-// }
-// bot_jeiz();
+    fetch(apiUrl, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      if (response.ok) {
+        console.log("Bot rodou");
+      }
+    });
+  }, 4000000);
+}
+bot_jeiz();
