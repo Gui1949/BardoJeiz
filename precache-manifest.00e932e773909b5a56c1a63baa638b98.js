6196a9cfad5d2688804d59caee86cde55636a59f
Gui1949 <guilhermesantiago12@gmail.com>
2022-01-30 14:05:03 -0300

    Updates

---
+++ precache-manifest.00e932e773909b5a56c1a63baa638b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569d02d54cfd81f33ee17a373acdf959",
+    "url": "/BardoJeiz/index.html"
+  },
+  {
+    "revision": "6f17be96f815372ce173",
+    "url": "/BardoJeiz/static/css/main.1abef669.chunk.css"
+  },
+  {
+    "revision": "6d921f82fd66874e8757",
+    "url": "/BardoJeiz/static/js/2.bf688a28.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/BardoJeiz/static/js/2.bf688a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f17be96f815372ce173",
+    "url": "/BardoJeiz/static/js/main.d19d9494.chunk.js"
+  },
+  {
+    "revision": "e731e144f62e5ad0d4c7",
+    "url": "/BardoJeiz/static/js/runtime-main.5641efc7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/BardoJeiz/static/media/logo.ee7cd8ed.svg"
+  }
+]);
