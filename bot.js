4a2669bad2efc5a77c521eb419037c8e3d87f60f
Gui1949 <guilhermesantiago12@gmail.com>
2022-03-27 16:16:40 -0300

    Blogueirinha V

--- bot.js
+++ bot.js
@@ -132,6 +132,74 @@ function bot_jeiz() {
     "quando fui pra portugal, arrumei uma briga muito feia com a mafia russa",
   ];
 
+  let imagem = [
+    "https://afabrica.bar/wp-content/uploads/2018/07/botequim-do-hugo.jpg",
+    "https://www.youtube.com/watch?v=6xa6F7ntzJU",
+    "https://www.clicrbs.com.br/imagesrc/5572057.jpg",
+    "https://images.redetv.uol.com.br/public/jornalismo/redetvinoticias/20200110155648NqUiNaXlhR.png",
+    "https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2015/10/varzea.jpg",
+    "https://imganuncios.mitula.net/chevrolet_chevette_1992_gasolina_chevrolet_chevette_lslsledlse_1_6_1992_gasolina_cinza_2490082599395995638.jpg",
+    "https://i.ytimg.com/vi/ajyXB2eQd-0/hqdefault.jpg",
+    "https://acervo.oglobo.globo.com/incoming/23270993-3db-b48/imagemVerticalFotogaleria/40380842_23061999DIVULGACAO-CROMOEXT-SXCANTOR-REGINALDO-ROSSI.jpg",
+    "https://64.media.tumblr.com/0cabf68dfb6413acf75179c3850356e1/tumblr_ml442vIeZP1so06jbo1_1280.jpg",
+    "https://4.bp.blogspot.com/-0ZoabEfNkCI/U5Nra3XaVRI/AAAAAAAAC0o/2bVy7OC1h5U/s1600/CINE+FLORIDA+-+GUARULHOS.jpg",
+    "https://i.ytimg.com/vi/ZoAFngyIuZY/hqdefault.jpg",
+    "https://i.ytimg.com/vi/StE0GLJmDu4/hqdefault.jpg",
+    "https://i.ytimg.com/vi/YVcUBoxYx1w/sddefault.jpg",
+    "https://vejario.abril.com.br/wp-content/uploads/2016/12/jboni1.jpg",
+    "https://www.futebolinterior.com.br/Imagem/2050467195/727x489",
+    "https://s2.glbimg.com/gedlN4QRYwb2srRKnG4dXBN-kUw=/s.glbimg.com/og/rg/f/original/2014/12/17/tv_de_tubo.jpg",
+    "https://i.ytimg.com/vi/ZUUC3O8XZ5c/maxresdefault.jpg",
+    "https://www.osaogoncalo.com.br/img/Artigo-Destaque/80000/1_marcos_oliveira_como_beicola_em_a_grande_familia_00087132_0.jpg?xid=236127",
+    "https://1.bp.blogspot.com/-KPMdzobvHtc/VziZKycsS2I/AAAAAAAADCE/ecYMC6knO80wD6dolQcjgOfkyTf9qpquwCLcB/s400/bebado.jpg",
+    "https://d1o6h00a1h5k7q.cloudfront.net/imagens/img_m/8044/3450347.jpg",
+    "https://miro.medium.com/max/3838/1*2P9GSHpGl64ZLwrwnOn9hQ.jpeg",
+    "https://pesqueiropontepreta.com.br/wp-content/uploads/2017/12/Peixe-Tambac%C3%BA.jpeg",
+    "https://i.ytimg.com/vi/rWghZZ8y7i0/mqdefault.jpg",
+    "https://www.youtube.com/watch?v=JyJfS7BpeDg",
+    "https://www.youtube.com/watch?v=SjU4u6C-eaA",
+    "https://www.youtube.com/watch?v=lR-kdXr9ZvM",
+    "https://www.youtube.com/watch?v=jwI1j7sslYI",
+    "https://www.youtube.com/watch?v=6a4pVMQ8pnY",
+    "https://www.youtube.com/watch?v=wWUPdA9n01o",
+    "https://www.youtube.com/watch?v=PpEABeH4dw8",
+    "https://www.youtube.com/watch?v=QWVdyVd79HI",
+    "https://www.youtube.com/watch?v=ZA5F2Z6gLzE",
+    "https://www.youtube.com/watch?v=wAeejHGBazg",
+    "https://www.youtube.com/watch?v=izGwDsrQ1eQ",
+    "https://www.youtube.com/watch?v=wPh-MK_5k_k",
+    "https://www.youtube.com/watch?v=CKTkigVUNBc",
+    "https://www.youtube.com/watch?v=H6wl-EyhXl0",
+    "https://www.youtube.com/watch?v=fUjOfsoBhMY",
+    "https://www.youtube.com/watch?v=8_7qkRwcMFc",
+    "https://www.youtube.com/watch?v=KRttQEyK4F8",
+    "https://i.pinimg.com/564x/b6/74/5e/b6745ef924d75e11eb4af59c88a99355.jpg",
+    "https://i.pinimg.com/736x/19/5f/0d/195f0dae8d35417d39cdf1ced4001f06.jpg",
+    "https://images3.memedroid.com/images/UPLOADED465/5c1f0af23834a.jpeg",
+    "https://images7.memedroid.com/images/UPLOADED598/57979854243c5.jpeg",
+    "https://www.notibras.com/site/wp-content/uploads/2018/03/cerveja.jpg",
+    "https://www.youtube.com/watch?v=KXkxznwWXug",
+    "https://www.youtube.com/watch?v=4XKGfziuw5c",
+    "https://www.youtube.com/watch?v=gbdqaDRhs84",
+    "https://www.youtube.com/watch?v=BtKg458XAHk",
+    "https://www.youtube.com/watch?v=Sc9j7YBcVO8",
+    "https://www.youtube.com/watch?v=gbdqaDRhs84",
+    "https://www.youtube.com/watch?v=bdXr_PiQoTc",
+    "https://www.youtube.com/watch?v=9fMPltVszpQ",
+    "https://www.youtube.com/watch?v=WUT6qnYw9Dc",
+    "https://www.youtube.com/watch?v=3CQfom3ceCk",
+    "https://www.youtube.com/watch?v=nXyHfv6XUSc",
+    "https://www.youtube.com/watch?v=uK62BM7pnMA",
+    "https://www.youtube.com/watch?v=qi0VCZhum2c",
+    "https://www.youtube.com/watch?v=hyGfCUVpD_k",
+    "https://www.youtube.com/watch?v=ItIMvLKeKME",
+    "https://www.youtube.com/watch?v=PoLKlQwwQTg",
+    "https://i.redd.it/u7rqhnmy7yc31.jpg",
+    "https://www.youtube.com/watch?v=Yc3Hff6WL9k",
+    "https://i.ytimg.com/vi/hyGfCUVpD_k/hqdefault.jpg",
+    "https://media.tenor.com/images/cea228dc3a8012d41fa7e0373ddbce2b/tenor.gif",
+  ];
+
   tipo_frase = getRandomInt(0, 10);
 
   console.table(tipo_frase);
@@ -184,7 +252,7 @@ function bot_jeiz() {
       });
     } catch (err) {
       console.log("Erro:", err);
-      input = "https://picsum.photos/500/500?random=" + [getRandomInt(0, 999)];
+      input = imagem[getRandomInt(0, imagem.length)]
       let userpic =
         "https://www.osaogoncalo.com.br/img/Artigo-Destaque/80000/1_marcos_oliveira_como_beicola_em_a_grande_familia_00087132_0.jpg?xid=236127";
       let name = "Jeiz";
@@ -221,11 +289,10 @@ function bot_dona_sonia() {
       "BOM DIA FLORES DO CAMPO, AMO TODOS VOCÊS",
     ];
 
-  
     desc = descricao_[getRandomInt(0, descricao_.length)];
-  
+
     let img_aleatoria = "";
-  
+
     async function puxar_img() {
       try {
         await client.search("bom dia gif").then((images) => {
@@ -235,25 +302,25 @@ function bot_dona_sonia() {
             "https://wp-cdn.etiquetaunica.com.br/blog/wp-content/uploads/2018/08/11122702/capa-post-top-blogueiras-brasileiras-compressed.jpg";
           let name = "Dona Sônia";
           let apiUrl = "https://bardojeiz-server.herokuapp.com/data/bot_upload";
-  
+
           if (img_aleatoria.includes("x-raw-image")) {
             throw new Error("001 - X-RAW IMAGE");
           }
-  
+
           let FormData = require("form-data");
           let data = new FormData();
-  
+
           data.append("photo", img_aleatoria);
           data.append("photo_pic", userpic);
           data.append("description", desc);
           data.append("username", name);
-  
+
           fetch(apiUrl, {
             method: "POST",
             body: data,
           }).then(function (response) {
             if (response.ok) {
-              console.log("Bot JEIZ rodou");
+              console.log("Bot sonia rodou");
             }
           });
         });
@@ -263,9 +330,6 @@ function bot_dona_sonia() {
     }
     puxar_img();
   }
-
-
-
 }
 
 function bot_blogueirinha() {
