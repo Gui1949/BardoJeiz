ea916d1bce716b68f275ac708f99830fd46e9a37
Gui1949 <guilhermesantiago12@gmail.com>
2023-11-03 18:23:42 -0300

    gerador de frases do jeiz corrigido

--- bot.js
+++ bot.js
@@ -17,44 +17,122 @@ function getRandomInt(min, max) {
 
 function bot_jeiz() {
   try {
-    let artigos = ["o"];
+    let getRandomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    let artigos = [
+      { palavra: "o", tipo: "masc" },
+      { palavra: "a", tipo: "fem" },
+    ];
 
     let sujeitos = [
-      "bar",
-      "jacksons",
-      "koerano",
-      "jogo do bicho",
-      "cara",
-      "cachaça",
-      "faca enferrujada",
-      "zap",
-      "ponte preta",
-      "fela da puta",
+      { palavra: "bar", tipo: "masc", cat: "obj" },
+      { palavra: "jacksons", tipo: "masc", cat: "pessoa" },
+      { palavra: "bolsonaro", tipo: "masc", cat: "pessoa" },
+      { palavra: "lula", tipo: "masc", cat: "pessoa" },
+      {
+        palavra: "desgraçado da vigilancia sanitaria",
+        tipo: "masc",
+        cat: "pessoa",
+      },
+      { palavra: "jairton kleyson", tipo: "masc", cat: "pessoa" },
+      { palavra: "filho da puta do bicheiro", tipo: "masc", cat: "pessoa" },
+      { palavra: "koerano", tipo: "masc", cat: "pessoa" },
+      { palavra: "bicheiro", tipo: "masc", cat: "obj" },
+      {
+        palavra: "cara que me deve quinze mil reais",
+        tipo: "masc",
+        cat: "pessoa",
+      },
+      {
+        palavra:
+          "cachaça falsificada que comprei semana passada daquele cara suspeito",
+        tipo: "fem",
+        cat: "obj",
+      },
+      {
+        palavra: "faca enferrujada que ta aqui no bar desde 1963",
+        tipo: "fem",
+        cat: "obj",
+      },
+      { palavra: "joão do bar vizinho", tipo: "masc", cat: "pessoa" },
     ];
 
     let verb_lig = [
-      "é",
-      "ta",
-      "parece",
-      "anda",
-      "vive",
-      "fica",
-      "continua",
-      "gosta",
+      { palavra: "é", cat: "obj" },
+      { palavra: "ta", cat: "obj" },
+      { palavra: "parece que ta", cat: "obj" },
+      { palavra: "anda", cat: "pessoa" },
+      { palavra: "vive", cat: "pessoa" },
+      { palavra: "continua", cat: "pessoa" },
     ];
 
     let pron_adj = [
-      "foda",
-      "filho da puta",
-      "corno",
-      "parecido com o reginaldo rossi",
-      "fi de rapariga",
-      "sujo",
-      "arrombado",
-      "cachaceiro",
-      "fudido",
-      "se fudendo",
+      { palavra: "foda", tipo: "ambos", cat: "ambos" },
+      {
+        palavra: "sendo um belo de um filho da puta",
+        tipo: "masc",
+        cat: "pessoa",
+      },
+      { palavra: "falando putaria nessa porra", tipo: "masc", cat: "pessoa" },
+      {
+        palavra:
+          "comendo traveco escondido e ainda fica disfarçando... a gente sabe de tudo",
+        tipo: "masc",
+        cat: "pessoa",
+      },
+      { palavra: "sendo corno", tipo: "masc", cat: "pessoa" },
+      {
+        palavra: "parecendo com o reginaldo rossi",
+        tipo: "masc",
+        cat: "pessoa",
+      },
+      {
+        palavra: "frequentando o cabaré da leila",
+        tipo: "ambos",
+        cat: "pessoa",
+      },
+      { palavra: "suja pra carai, puta que pariu", tipo: "fem", cat: "obj" },
+      { palavra: "sujo que só a porra viu", tipo: "masc", cat: "obj" },
+      { palavra: "muito ruim viu puta que pariu", tipo: "ambos", cat: "obj" },
+      { palavra: "sendo muito arrombado", tipo: "masc", cat: "pessoa" },
+      {
+        palavra:
+          "cachaceiro, não paga porra nenhuma e rouba na sinuca... ta fudido",
+        tipo: "masc",
+        cat: "pessoa",
+      },
+      { palavra: "muito fudido pra carai", tipo: "masc", cat: "ambos" },
+      {
+        palavra: "só se fudendo, parece puta esse corno",
+        tipo: "masc",
+        cat: "pessoa",
+      },
     ];
+
+    let artigo = artigos[getRandomInt(0, artigos.length)];
+
+    //Filtra sujeito pelo tipo (masc ou fem)
+
+    let sujeito = sujeitos.filter((suj) => suj.tipo == artigo.tipo);
+    sujeito = sujeito[getRandomInt(0, sujeito.length)];
+
+    //Filtra o verbo de ligação pelo tipo
+
+    let verbo = verb_lig.filter(
+      (ver) => ver.cat == sujeito.cat || sujeito.cat == "ambos"
+    );
+    verbo = verbo[getRandomInt(0, verbo.length)];
+
+    //Filta o adjetivo com o verbo de ligação e sujeito
+
+    let adj = pron_adj.filter(
+      (pro) =>
+        (pro.cat == verbo.cat || verbo.cat == "ambos") &&
+        (pro.tipo == sujeito.tipo || pro.tipo == "ambos")
+    );
+    adj = adj[getRandomInt(0, adj.length)];
 
     let descricao = [
       "hoje ta foda aqui rapaziada",
@@ -144,13 +222,13 @@ function bot_jeiz() {
 
     if (tipo_frase <= 5) {
       desc =
-        artigos[getRandomInt(0, artigos.length)] +
+        artigo.palavra +
         " " +
-        sujeitos[getRandomInt(0, sujeitos.length)] +
+        sujeito.palavra +
         " " +
-        verb_lig[getRandomInt(0, verb_lig.length)] +
+        verbo.palavra +
         " " +
-        pron_adj[getRandomInt(0, pron_adj.length)];
+        adj.palavra;
     } else {
       desc = descricao[getRandomInt(0, descricao.length)];
     }
@@ -1544,7 +1622,7 @@ function bot_bitcoin() {
 //           dados = parseFloat(dados).toFixed(2);
 //           console.log(dados);
 
-//           var FormData = require("form-data");
+//           var FormData = require("form-data")f;
 //           var data = new FormData();
 
 //           let apiUrl = "https://bar-do-jeiz.onrender.com/data/bot_upload";
@@ -1597,11 +1675,12 @@ setInterval(bot_anime, 700000);
 
 setInterval(bot_dona_sonia, 700000);
 
+bot_jeiz();
+
 bot_merchan();
 bot_dona_sonia();
 bot_gringo();
 bot_bitcoin();
-bot_jeiz();
 bot_g1();
 bot_petista();
 bot_blogueirinha();
